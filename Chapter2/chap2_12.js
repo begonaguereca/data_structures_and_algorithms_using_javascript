fd9f456af223aacdbbd7fe43420e6ff58805349c
Begona Guereca <begonaguereca@gmail.com>
2018-01-30 11:41:16 -0800

    solved chap 2 problem

--- Chapter2/chap2_12.js
+++ Chapter2/chap2_12.js
@@ -1,13 +1,26 @@
-var grades = [[89, 77],[76, 82, 81],[91, 94, 89, 99]];
-var total = 0;
-var average = 0.0;
-for (var row = 0; row < grades.length; ++row) {
-   for (var col = 0; col < grades[row].length; ++col) {
-      total += grades[row][col];
-   }
-   average = total / grades[row].length;
-   print("Student " + parseInt(row+1) + " average: " +    
-         average.toFixed(2));
-   total = 0;
-   average = 0.0;
-}
+const GradeBook = () => {
+  this.grades = {};
+};
+
+
+GradeBook.prototype.addGrade = (student, grade) => {
+  if (this.grades.student) {
+    this.grades.student.push(grade);
+  } else {
+    this.grades.student = [grade];
+  }
+};
+
+
+GradeBook.prototype.averageGrade = (student) => {
+  if (this.grades.student) {
+    var sum = this.grades.student.reduce(function(a,b){
+      return a + b;
+    });
+
+    return sum/this.grades.student.length;
+  } else {
+
+    return 'This student is not in your gradebook';
+  }
+};
